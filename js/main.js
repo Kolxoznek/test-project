24a7fd153f7fbf4077d9702fce03f2e3799ca326
evgeniy <example@gmail.com>
2023-10-30 16:48:07 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// const al = alert('Hello!')
+// const conf = confirm('Are you okay?')
+// const ans = prompt('asdasd?', '18')
+
+// console.log(al)
+// console.log(conf)
+// console.log(ans)
+
+const answers = []
+
+answers[0] = prompt('1?', '')
+answers[1] = prompt('2?', '')
+answers[2] = prompt('3?', '')
+
+document.write(answers)
+console.log(typeof(answers))
+console.log(answers)
+
+
+
+
